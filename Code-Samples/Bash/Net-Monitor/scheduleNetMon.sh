ac458d49584e598e6dca72cce56e90469257a8c7
Daniel Schlatter <djschlatt@gmail.com>
2021-07-14 18:05:26 -0600

    Update and rename scheduleNetMon-1.sh to scheduleNetMon.sh

--- Code-Samples/Bash/Net-Monitor/scheduleNetMon.sh
+++ Code-Samples/Bash/Net-Monitor/scheduleNetMon.sh
@@ -3,7 +3,6 @@
 # Class: CSCI 5742 Cybersecurity Programming
 # Name: Daniel Schlatter & Angela Mcneese
 # Date: 03/06/2021
-# Assignment: HW3 Part 2
 # Description: Schedule a cronjob for the NetMon.sh script every five minutes. 
 
 tmpfile='temp.txt'
